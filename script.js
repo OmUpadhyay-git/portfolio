76a3e56f08ec44a15ab1310682c7ec3ad325c732
Your Name <you@example.com>
2025-12-23 11:11:21 +0530

    js

---
+++ script.js
@@ -0,0 +1,14 @@
+const slides = document.querySelectorAll(".slide");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("active");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+slides.forEach(slide => observer.observe(slide));
